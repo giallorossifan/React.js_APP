84603ba416d460cf9fc12dfb109dfffdbe997d68
Claudio Salas <djsalas2133@gmail.com>
2023-01-10 19:25:55 -0800

    added StarWars Character random generator API

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,9 +6,54 @@ import Item from '/src/myitem';
 import React from 'react';
 
 class StarWars extends React.Component{
+  constructor() {
+    super ()
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      gender: null,
+    }
+  }
+  getNewCharacter() {
+    const randomNumber = Math.round(Math.random() * 82)
+    // below is a template literal
+    const url = `https://swapi.dev/api/people/${randomNumber}/`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+      this.setState({
+      name: data.name,
+      height: data.height,
+      gender: data.gender,
+      loadedCharacter: true,
+     })
+    })
+
+  }
+
   render(){
     return (
-      <h1> May the Force Be With You </h1>
+      <div>
+        {
+          this.state.loadedCharacter &&
+          <div>
+            <h3> May the Force Be With You </h3>
+            <h3> {this.state.name} </h3>
+            <h3> {this.state.height} cm </h3>
+            <h3> {this.state.gender} </h3>
+          </div>
+
+        }
+
+        <button type="button"
+          onClick={() => this.getNewCharacter()}
+          className="btn">
+            Random StarWars Character
+        </button>
+
+      </div>
+
     )
   }
 }
@@ -34,10 +79,16 @@ function App() {
         </button>
         <p>
           Edit <Item name="Leia Bear" /><code>src/App.jsx</code> and save to test HMR
-          <Item name="Lisa Bear" />
-          <Item name="Ella Bear" />
+          {/* <Item name="Lisa Bear" /> */}
+          {/* <Item name="Ella Bear" /> */}
         </p>
-        <StarWars />
+
+
+        <div>
+          <StarWars />
+        </div>
+
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
